b1948ab5a60e4902ad001a5a61b0103238134435
Dipansh Parmar <dipanshparmar870@gmail.com>
2022-12-20 20:18:45 +0530

    added messages for every type of score

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:countries/pages/result_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './pages/pages.dart';
--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,9 +1,46 @@
 import 'package:flutter/material.dart';
 
+import '../utils/functions/functions.dart';
+
 class ResultPage extends StatelessWidget {
   const ResultPage({super.key});
 
   static const String routeName = '/result-page';
+
+  // method to get a message according to the score
+  String? getMessage(int score, int total) {
+    // getting the percentage amount
+    double percentage = getPercentage(score, total);
+
+    // getting the percentage in scale of 10
+    int inTenScale = percentage ~/ 10;
+
+    // returning a message according to the ten scale
+    if (inTenScale == 1) {
+      return 'That\'s a good start.';
+    } else if (inTenScale == 2) {
+      return 'Climbing the stairs just like that.';
+    } else if (inTenScale == 3) {
+      return 'Got 25% answers correct.';
+    } else if (inTenScale == 4) {
+      return 'Nice! Almost reached the half.';
+    } else if (inTenScale == 5) {
+      return 'Woohoo! You got half of the answers correct.';
+    } else if (inTenScale == 6) {
+      return 'Wonderful.';
+    } else if (inTenScale == 7) {
+      return 'Impressive';
+    } else if (inTenScale == 8) {
+      return 'You deserve a treat today.';
+    } else if (inTenScale == 9) {
+      return 'That\'s some insanity right there.';
+    } else if (inTenScale == 10) {
+      return 'Don\'t you have a life?';
+    }
+
+    // otherwise just return null
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +50,8 @@ class ResultPage extends StatelessWidget {
     // extracting the data
     final int correctAnswers = data['correct'];
     final int totalQuestions = data['total'];
+
+    final String? message = getMessage(correctAnswers, totalQuestions);
 
     return Scaffold(
       appBar: AppBar(
@@ -35,15 +74,18 @@ class ResultPage extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'That\'s some insanity right there',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+            // if we have a message then show it
+            if (message != null)
+              const SizedBox(
+                height: 20,
               ),
-            ),
+            if (message != null)
+              Text(
+                message,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
           ],
         ),
       ),

5ece32c7a20a430c1032dbb881368d914bfeb588
Dipansh Parmar <dipanshparmar870@gmail.com>
2022-12-23 13:24:21 +0530

    optimized

--- lib/widgets/options.dart
+++ lib/widgets/options.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import '../utils/enums/enums.dart';
-import 'package:provider/provider.dart';
-import '../providers/providers.dart';
+import './widgets.dart';
 
-class Options extends StatefulWidget {
+class Options extends StatelessWidget {
   const Options({
     required this.items,
     required this.filterType,
@@ -14,71 +13,22 @@ class Options extends StatefulWidget {
   final FilterType filterType;
 
   @override
-  State<Options> createState() => _OptionsState();
-}
-
-class _OptionsState extends State<Options> {
-  @override
   Widget build(BuildContext context) {
-    return Consumer<CountriesProvider>(
-      builder: ((context, value, child) {
-        // getting the active filters
-        final Map activeFilters = value.getActiveFilters();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Wrap(
+        runSpacing: 10,
+        spacing: 10,
+        children: items.asMap().keys.map((e) {
+          // getting the current text value
+          final String textValue = items[e];
 
-        // getting the list of the active values according to the type
-        List values = activeFilters[widget.filterType];
-
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Wrap(
-            runSpacing: 10,
-            spacing: 10,
-            children: widget.items.asMap().keys.map((e) {
-              // getting the current text value
-              final String textValue = widget.items[e];
-
-              // is selected
-              final bool isSelected = values.contains(textValue);
-
-              return GestureDetector(
-                onTap: () {
-                  setState(() {
-                    if (!isSelected) {
-                      // applying the filter
-                      value.applyFilter(widget.filterType, textValue);
-                    } else {
-                      // removing the filter
-                      value.removeFilter(widget.filterType, textValue);
-                    }
-                  });
-                },
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 12,
-                  ),
-                  decoration: BoxDecoration(
-                    color: isSelected
-                        ? Theme.of(context).colorScheme.secondary
-                        : null,
-                    border: Border.all(
-                      color: Theme.of(context).colorScheme.secondary,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text(
-                    widget.items[e],
-                    style: TextStyle(
-                      color: isSelected ? Colors.black : Colors.white,
-                    ),
-                  ),
-                ),
-              );
-            }).toList(),
-          ),
-        );
-      }),
+          return Option(
+            filterType: filterType,
+            textValue: textValue,
+          );
+        }).toList(),
+      ),
     );
   }
 }
